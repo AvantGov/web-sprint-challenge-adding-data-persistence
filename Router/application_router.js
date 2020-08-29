6c1222e384b75cc86386104e9ee249b9b2336a01
avantgov <alexcouts18@gmail.com>
2020-08-29 11:52:21 -0500

    added knex configurations for db setup with express, ran knex migrations to create directory, tweaked project db name to override inital nomenclature created out of hestiation

---
+++ Router/application_router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const knex = require("knex");
+
+const database_access = require("../data/config");
+
+const router = express.Router();
+
+
+module.exports = router
--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
       client: 'sqlite3',
       useNullAsDefault: true,
       connection: {
-        filename: './data/projects_check.db3',
+        filename: './data/projects.db3',
       },
       migrations: {
         directory: './data/migrations'
